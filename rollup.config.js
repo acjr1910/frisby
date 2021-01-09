aa83daa74412f02554dc9f451b3a723f899f6cfb
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2021-01-09 18:02:51 -0300

    chore: remove UMD output config

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,8 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
-import babel from "@rollup/plugin-babel";
-
-import pkg from "./package.json";
 
 const input = ["src/index.js"];
 
 export default [
-  {
-    // UMD
-    input,
-    plugins: [
-      nodeResolve(),
-      babel({
-        babelHelpers: "bundled",
-      }),
-      terser(),
-    ],
-    output: {
-      file: `dist/${pkg.name}.min.js`,
-      format: "umd",
-      name: "frisby-fp",
-      esModule: false,
-      exports: "named",
-      sourcemap: true,
-    },
-  },
-  // ESM and CJS
   {
     input,
     plugins: [nodeResolve()],
