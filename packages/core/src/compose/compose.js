703307220e35d46a310753db110fd06c0cba87c4
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2021-01-12 13:27:22 -0300

    refactor: add commented compose function with better performance, using recursion

--- packages/core/src/compose/compose.js
+++ packages/core/src/compose/compose.js
@@ -6,3 +6,19 @@ export default function compose(...fns) {
     return result
   }
 }
+
+// faster alternative;
+// export function compose2(...fns) {
+//   let lastFnNdx = fns.length - 1
+
+//   return function composedFn(result) {
+//     if (!fns) return result
+
+//     if (lastFnNdx < 1) return fns[0](result)
+
+//     const lastFnIndex = lastFnNdx
+//     lastFnNdx--
+
+//     return composedFn(fns[lastFnIndex](result))
+//   }
+// }
