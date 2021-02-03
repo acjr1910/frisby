c2cc6c4d3af6df167ebb763e0e538ea4a096c845
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2021-02-02 22:50:20 -0300

    remove: removing comments from file

--- packages/core/src/compose/compose.js
+++ packages/core/src/compose/compose.js
@@ -6,19 +6,3 @@ export default function compose(...fns) {
     return result
   }
 }
-
-// faster alternative;
-// export function compose2(...fns) {
-//   let lastFnNdx = fns.length - 1
-
-//   return function composedFn(result) {
-//     if (!fns) return result
-
-//     if (lastFnNdx < 1) return fns[0](result)
-
-//     const lastFnIndex = lastFnNdx
-//     lastFnNdx--
-
-//     return composedFn(fns[lastFnIndex](result))
-//   }
-// }
