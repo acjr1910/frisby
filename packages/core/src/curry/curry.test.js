4ebceee8e71951c2f874873a4e8c759efbef8ee0
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2021-02-16 19:56:45 -0300

    tests: curry new implementation

--- packages/core/src/curry/curry.test.js
+++ packages/core/src/curry/curry.test.js
@@ -1,28 +1,18 @@
 import curry from './index'
 
 describe('curry', () => {
-  it('should curry sum function with 3 args', () => {
-    function sumOf3(a, b, c) {
-      return a + b + c
-    }
-
-    const curriedFn = curry(sumOf3)
-    curriedFn(1)
-    curriedFn(2)
-    const result = curriedFn(3)
-
-    expect(result).toBe(6)
+  it('should curry sumOf3(a, b, c) function', () => {
+    const sumOf3 = (a, b, c) => a + b + c
+    const curriedFn = curry(sumOf3)(1)
+    const result = curriedFn(2)
+    expect(result(3)).toBe(6)
   })
 
-  it('should curry function with 2 args', () => {
-    function concatStr(str1, str2) {
-      return str1 + ' ' + str2
-    }
-
+  it('should curry concatStr(str1, str2) function', () => {
+    const concatStr = (str1, str2) => str1 + ' ' + str2
     const curriedFn = curry(concatStr)
-    curriedFn('Hello')
-    const result = curriedFn('World!')
-
+    const helloWorld = curriedFn('Hello')
+    const result = helloWorld('World!')
     expect(result).toBe('Hello World!')
   })
 })
