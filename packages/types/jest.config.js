addc1a09b71d1367c53fcacdbed8a97172856100
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2021-03-08 21:47:36 -0300

    chore(all): config coverage folder and add it to .gitignore file

--- packages/types/jest.config.js
+++ packages/types/jest.config.js
@@ -3,4 +3,11 @@ const packageJson = require('./package.json')
 module.exports = {
   name: packageJson.name,
   displayName: packageJson.name,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!**/*.config.js',
+  ],
 }
