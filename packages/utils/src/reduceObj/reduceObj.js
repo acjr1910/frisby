14b5f84ee15211cb06968ec35d0e01175f2ff8d1
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2021-04-20 23:26:33 -0300

    docs(utils): add reduceObj jsdoc

--- packages/utils/src/reduceObj/reduceObj.js
+++ packages/utils/src/reduceObj/reduceObj.js
@@ -1,9 +1,15 @@
 import { curry } from '@frisby/core'
 
-function reduceObj(reducerFn, initialValue, o) {
+/**
+ * Reduces an object (HashMap)
+ * @param {(acc,curr) => {}} reducerFn - Reducer function
+ * @param {Object} initialValue - Reducer initial value
+ * @param {Object} objToReduce - Object to be reduced
+ */
+function reduceObj(reducerFn, initialValue, objToReduce) {
   let result = initialValue
-  const keys = Object.keys(o)
-  keys.map((key) => (result = reducerFn(result, o[key])))
+  const keys = Object.keys(objToReduce)
+  keys.map((key) => (result = reducerFn(result, objToReduce[key])))
   return result
 }
 
